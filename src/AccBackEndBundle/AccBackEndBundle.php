c62241c7348f45fbc37c174e7d4136ef0b02f053
ilya-martynyuk <mescalito1990@gmail.com>
2015-12-02 16:00:25 +0200

    Added backend API bundle

---
+++ src/AccBackEndBundle/AccBackEndBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AccBackEndBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AccBackEndBundle extends Bundle
+{
+}
---
+++ src/AccBackEndBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AccBackEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AccBackEndBundle:Default:index.html.twig');
+    }
+}
