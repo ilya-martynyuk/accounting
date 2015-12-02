5abacca9aab0293acc773e53b53b23736fdd8464
ilya-martynyuk <mescalito1990@gmail.com>
2015-12-02 16:19:03 +0200

    Corrected structure of tests folders.

---
+++ src/AppBackEndBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBackEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBackEndBundle:Default:index.html.twig');
+    }
+}
